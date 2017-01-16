ac217d9aaacfdd933a531d6bc56e56e513d7da33
vivianhoang <vivtsehoang@gmail.com>
2017-01-15 17:06:06 -0800

    Completed Day 7. (Edited)

--- 30-Day-Challenge/Day_7-Arrays.py
+++ 30-Day-Challenge/Day_7-Arrays.py
@@ -4,10 +4,24 @@ Given an array of integers, print the elements in reverse order as
 a single line of space-separated numbers.
 """
 
-n = int(raw_input().strip())
-arr = map(int,raw_input().strip().split(' '))
+n = int(input())
+name_numbers = []
+phone_book = {}
+count = 0
 
-rev_arr = arr[::-1]
+for i in range(n):
+    name_numbers.append(raw_input().split(" "))
 
-for i in rev_arr:
-    print i,
+for j in name_numbers:
+    phone_book[name_numbers[count][0]] = name_numbers[count][1]
+    count += 1
+
+while True:
+    try:
+        name = raw_input()
+        if name in phone_book:
+            print('%s=%s' % (name, phone_book[name]))
+        else:
+            print('Not found')
+    except:
+        break
